cfa4aac8477dffe2d1ff13477d17b164b9716f42
BugHunter-X <askzenai@gmail.com>
2025-01-08 13:08:06 +0100

    Add index.solution.js file

---
+++ index.solution.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+//The solution is often related to the _app.js file or next.config.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a test paragraph.</p>
+    </div>
+  );
+}
+```
